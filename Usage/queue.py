4d21b77d63fd07c3e868201e7ce251c738ecb2fe
Duc Filan <ducfilan@gmail.com>
2017-11-18 16:23:36 +0700

    Queue

---
+++ Usage/queue.py
@@ -0,0 +1,18 @@
+import unittest
+from Implementation.queue import Queue
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        queue = Queue()
+        self.assertEqual(queue.is_empty(), True)
+        queue.enqueue(1)
+        queue.enqueue("two")
+        queue.enqueue(True)
+        self.assertEqual(queue.size(), 3)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.size(), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
