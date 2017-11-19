2cae3666b44cf0adee3a4732d83fc71b89bf00ed
Duc Filan <ducfilan@gmail.com>
2017-11-19 18:03:05 +0700

    Add  more cases to Deque

--- Usage/deque.py
+++ Usage/deque.py
@@ -5,12 +5,16 @@ from Implementation.deque import Deque
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         d = Deque()
+        self.assertEqual(d.is_empty(), True)
         d.add_front('hello')
         d.add_rear('world')
         self.assertEqual(d.size(), 2)
         self.assertEqual(d.remove_front(), 'hello')
-        self.assertEqual(d.remove_rear(), 'world')
-        self.assertEqual(d.size(), 0)
+        d.add_front(1)
+        self.assertEqual(d.remove_front(), 1)
+        d.add_rear(2)
+        self.assertEqual(d.remove_rear(), 2)
+        self.assertEqual(d.size(), 1)
 
 
 if __name__ == '__main__':
