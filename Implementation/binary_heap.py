2e0acccd408daaded1bb7b3dba9c42842a2012ff
Duc Filan <ducfilan@gmail.com>
2018-01-07 23:31:27 +0700

    Update binary heap

--- Implementation/binary_heap.py
+++ Implementation/binary_heap.py
@@ -14,3 +14,25 @@ class BinaryHeap(object):
         self.heap_list.append(k)
         self.current_size += 1
         self.perc_up(self.current_size)
+
+    def perc_down(self, i):
+        while (i * 2) <= self.current_size:
+            min_child = self.min_child(i)
+            if self.heap_list[i] > self.heap_list[min_child]:
+                self.heap_list[i], self.heap_list[min_child] = self.heap_list[min_child], self.heap_list[i]
+            i = min_child
+
+    def min_child(self, i):
+        if i * 2 + 1 > self.current_size:
+            return i * 2
+        else:
+            if self.heap_list[i * 2] < self.heap_list[i * 2 + 1]:
+                return i * 2
+            return i * 2 + 1
+
+    def del_min(self):
+        del_val = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.heap_list.pop()
+        self.current_size -= 1
+        return del_val
