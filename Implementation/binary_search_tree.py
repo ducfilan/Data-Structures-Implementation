92451b01a7d23d38a7a95dbc7d013b313d1b226a
Duc Filan <ducfilan@gmail.com>
2018-01-09 17:23:27 +0700

    Update binary heap tests

---
+++ Implementation/binary_search_tree.py
@@ -0,0 +1,3 @@
+class TreeNode(object):
+    def __init__(self):
+        pass
--- Usage/binary_heap.py
+++ Usage/binary_heap.py
@@ -6,13 +6,11 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         h = BinaryHeap()
         h.build_heap([9, 5, 6, 2, 3, 7, 11, 22, 43, 1, 4, 50, 13, 9, 13, 12, 3, 4, 2, 5, 1, 3, 1, 3, 4])
-        h.output_heap()
+        self.assertEqual(' '.join(map(str, h.heap_list)), '0 1 1 3 2 1 4 9 3 2 5 3 6 13 11 13 12 22 4 43 5 9 3 4 50 7')
         h.del_min()
-        h.output_heap()
+        self.assertEqual(' '.join(map(str, h.heap_list)), '0 1 1 3 2 3 4 9 3 2 5 3 6 13 11 13 12 22 4 43 5 9 7 4 50')
         h.insert(2)
-        h.output_heap()
-
-        self.assertEqual(True, True)
+        self.assertEqual(' '.join(map(str, h.heap_list)), '0 1 1 2 2 3 3 9 3 2 5 3 4 13 11 13 12 22 4 43 5 9 7 4 50 6')
 
 
 if __name__ == '__main__':
