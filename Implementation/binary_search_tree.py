08910c663d52db2a005ed6cf092f8e9b2a1d9217
Duc Filan <ducfilan@gmail.com>
2018-01-13 17:52:09 +0700

    Add binary search tree

--- Implementation/binary_search_tree.py
+++ Implementation/binary_search_tree.py
@@ -39,3 +39,38 @@ class TreeNode(object):
             self.left_child.parent = self
         if self.has_right_child():
             self.right_child.parent = self
+
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+
+        self.size += 1
+
+    def _put(self, key, val, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, val, self.left_child)
+            else:
+                current_node.left_child = TreeNode(key, val, parent=current_node)
+        else:
+            if current_node.has_right_child():
+                self._put(key, val, self.right_child)
+            else:
+                current_node.right_child = TreeNode(key, val, parent=current_node)
