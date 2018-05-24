aa01edfd132ed35c86907a66da527decd29a4aa1
Duc Filan <ducfilan@gmail.com>
2018-05-25 00:28:35 +0900

    Update Trie

--- Implementation/Trie.py
+++ Implementation/Trie.py
@@ -1,9 +1,9 @@
 class TrieNode:
-    def __init__(self, val):
+    def __init__(self, val, is_end=False):
         self.value = val
-        self.is_end = False
+        self.is_end = is_end
         self.connected_nodes = []
-        
+
     def set_connected_to_node(self, new_node):
         self.connected_nodes.append(new_node)
 
@@ -13,27 +13,46 @@ class TrieNode:
     def get_value(self):
         return self.value
 
+    def set_is_end(self, is_end):
+        self.is_end = is_end
+
+
 root = TrieNode('*')
 
 words = ["cost", "cast", "constant", "love", "lost", "last"]
 
+
 def build_trie(root, word):
-    current_parrent_node = root
-    nodes_dict = { node.get_value(): node for node in current_parrent_node.get_connected_nodes() }
-    
-    for c in word:
+    current_parent_node = root
+    nodes_dict = {node.get_value(): node for node in current_parent_node.get_connected_nodes()}
+
+    for i, c in enumerate(word):
         if c not in nodes_dict:
-            new_node = TrieNode(c)
-            current_parrent_node.set_connected_to_node(TrieNode(c))
-            current_parrent_node = new_node
+            new_node = TrieNode(c, i + 1 == len(word))
+            current_parent_node.set_connected_to_node(new_node)
+            current_parent_node = new_node
         else:
-            current_parrent_node = nodes_dict[c]
+            if i + 1 == len(word):
+                nodes_dict[c].set_is_end(True)
+
+            current_parent_node = nodes_dict[c]
+
 
 for word in words:
     build_trie(root, word)
 
-for x in root.get_connected_nodes():
-    print(x.get_value())
-    for y in x.get_connected_nodes():
-        print(y.get_value())
-		
+
+def find_words_start_with(prefix):
+    words = []
+
+    current_parent_node = root
+    nodes_dict = {node.get_value(): node for node in current_parent_node.get_connected_nodes()}
+
+    for i, c in enumerate(prefix):
+        if c in nodes_dict:
+            current_parent_node = nodes_dict[c]
+
+    for node in current_parent_node.get_connected_nodes():
+        words.append(prefix + node.get_value())
+
+    return words
