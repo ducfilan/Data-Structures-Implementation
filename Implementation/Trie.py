b03f556e9608b0fd57f36c8955404da2274b4242
ducfilan <ducfilan@gmail.com>
2018-05-28 16:12:54 +0900

    Update Trie

--- Implementation/Trie.py
+++ Implementation/Trie.py
@@ -8,69 +8,67 @@ class TrieNode:
 		self.connected_nodes.append(new_node)
 
 
-root = TrieNode('*')
+class Trie:
+	def __init__(self, root):
+		self.root = root
 
-words = [
+	def build(self, words):
+		for word in words:
+			current_parent_node = self.root
+
+			for i, c in enumerate(word):
+				nodes_dict = {
+				    node.value: node
+				    for node in current_parent_node.connected_nodes
+				}
+
+				if c not in nodes_dict:
+					new_node = TrieNode(c, i + 1 == len(word))
+					current_parent_node.set_connected_to_node(new_node)
+					current_parent_node = new_node
+				else:
+					if i + 1 == len(word):
+						nodes_dict[c].is_end = True
+
+					current_parent_node = nodes_dict[c]
+
+	def find_words_start_with(self, prefix):
+		words = []
+
+		def dfs(node, word=''):
+			if not word:
+				word = prefix[:-1]
+
+			word += node.value
+
+			children = node.connected_nodes
+
+			if node.is_end:
+				words.append(word)
+
+			if not children:
+				word = ''
+			else:
+				for child in children:
+					dfs(child, word)
+
+		start_node = self.root
+
+		for c in prefix:
+			start_node = next(filter(lambda node: node.value == c, start_node.connected_nodes), None)
+			if not start_node:
+				return []
+
+		dfs(start_node)
+
+		return words
+
+
+trie = Trie(TrieNode('*'))
+
+trie.build([
     "castle", "cost", "cast", "constant", "love", "lost", "last", "less",
     "lotus", "lasting"
-]
+])
 
-
-def build_trie(root, word):
-	current_parent_node = root
-
-	for i, c in enumerate(word):
-		nodes_dict = {
-		    node.value: node
-		    for node in current_parent_node.connected_nodes
-		}
-
-		if c not in nodes_dict:
-			new_node = TrieNode(c, i + 1 == len(word))
-			current_parent_node.set_connected_to_node(new_node)
-			current_parent_node = new_node
-		else:
-			if i + 1 == len(word):
-				nodes_dict[c].is_end = True
-
-			current_parent_node = nodes_dict[c]
-
-
-for word in words:
-	build_trie(root, word)
-
-
-def find_words_start_with(prefix):
-	words = []
-
-	def dfs(node, word=''):
-		if not word:
-			word = prefix[:-1]
-
-		word += node.value
-
-		children = node.connected_nodes
-
-		if node.is_end:
-			words.append(word)
-
-		if not children:
-			word = ''
-		else:
-			for child in children:
-				dfs(child, word)
-
-	start_node = root
-
-	for c in prefix:
-		start_node = next(
-		    filter(lambda node: node.value == c, start_node.connected_nodes),
-		    None)
-
-	dfs(start_node)
-
-	return words
-
-
-for word in find_words_start_with('c'):
-	print(word)
+print(*trie.find_words_start_with('c'))
