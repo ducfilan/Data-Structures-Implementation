ee0786a88480dd9703e85e57a0a09416d7909745
Duc Filan <ducfilan@gmail.com>
2018-05-29 00:18:49 +0900

    Update Trie - Fix typo

--- Implementation/Trie.py
+++ Implementation/Trie.py
@@ -1,66 +1,66 @@
 class TrieNode:
-	def __init__(self, val, is_end=False):
-		self.value = val
-		self.connected_nodes = []
-		self.is_end = is_end
+    def __init__(self, val, is_end=False):
+        self.value = val
+        self.connected_nodes = []
+        self.is_end = is_end
 
-	def set_connected_to_node(self, new_node):
-		self.connected_nodes.append(new_node)
+    def set_connected_to_node(self, new_node):
+        self.connected_nodes.append(new_node)
 
 
 class Trie:
-	def __init__(self, root):
-		self.root = root
+    def __init__(self, root):
+        self.root = root
 
-	def build(self, dictionary):
-		for word in dictionary:
-			current_parent_node = self.root
+    def build(self, dictionary):
+        for word in dictionary:
+            current_parent_node = self.root
 
-			for i, c in enumerate(word):
-				nodes_dict = {
-				    node.value: node
-				    for node in current_parent_node.connected_nodes
-				}
+            for i, c in enumerate(word):
+                nodes_dict = {
+                    node.value: node
+                    for node in current_parent_node.connected_nodes
+                }
 
-				if c not in nodes_dict:
-					new_node = TrieNode(c, i + 1 == len(word))
-					current_parent_node.set_connected_to_node(new_node)
-					current_parent_node = new_node
-				else:
-					if i + 1 == len(word):
-						nodes_dict[c].is_end = True
+                if c not in nodes_dict:
+                    new_node = TrieNode(c, i + 1 == len(word))
+                    current_parent_node.set_connected_to_node(new_node)
+                    current_parent_node = new_node
+                else:
+                    if i + 1 == len(word):
+                        nodes_dict[c].is_end = True
 
-					current_parent_node = nodes_dict[c]
+                    current_parent_node = nodes_dict[c]
 
-	def find_words_start_with(self, prefix):
-		words = []
+    def find_words_start_with(self, prefix):
+        words = []
 
-		def dfs(node, word=''):
-			if not word:
-				word = prefix[:-1]
+        def dfs(node, word=''):
+            if not word:
+                word = prefix[:-1]
 
-			word += node.value
+            word += node.value
 
-			children = node.connected_nodes
+            children = node.connected_nodes
 
-			if node.is_end:
-				words.append(word)
+            if node.is_end:
+                words.append(word)
 
-			if not children:
-				word = ''
-			else:
-				for child in children:
-					dfs(child, word)
+            if not children:
+                word = ''
+            else:
+                for child in children:
+                    dfs(child, word)
 
-		start_node = self.root
+        start_node = self.root
 
-		for c in prefix:
-			start_node = next(
-			    filter(lambda node: node.value == c,
-			           start_node.connected_nodes), None)
-			if not start_node:
-				return []
+        for c in prefix:
+            start_node = next(
+                filter(lambda node: node.value == c,
+                       start_node.connected_nodes), None)
+            if not start_node:
+                return []
 
-		dfs(start_node)
+        dfs(start_node)
 
-		return words
+        return words
--- Usage/trie.py
+++ Usage/trie.py
@@ -1,25 +1,25 @@
 import unittest
-from Implementation.trie import TrieNode, Trie
+from Implementation.Trie import TrieNode, Trie
 
 
 class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		trie = Trie(TrieNode('*'))
+    def test_something(self):
+        trie = Trie(TrieNode('*'))
 
-		dictionary = [
-		    "castle", "cost", "cast", "constant", "lover", "lotus smile",
-		    "love", "lost", "last", "less", "lotus", "lasting"
-		]
-		trie.build(dictionary)
+        dictionary = [
+            "castle", "cost", "cast", "constant", "lover", "lotus smile",
+            "love", "lost", "last", "less", "lotus", "lasting"
+        ]
+        trie.build(dictionary)
 
-		self.assertEqual(
-		    trie.find_words_start_with('lo'),
-		    ['love', 'lover', 'lotus', 'lotus smile', 'lost'])
-		self.assertEqual(
-		    trie.find_words_start_with('c'),
-		    ['cast', 'castle', 'cost', 'constant'])
-		self.assertEqual(trie.find_words_start_with('cs'), [])
+        self.assertEqual(
+            trie.find_words_start_with('lo'),
+            ['love', 'lover', 'lotus', 'lotus smile', 'lost'])
+        self.assertEqual(
+            trie.find_words_start_with('c'),
+            ['cast', 'castle', 'cost', 'constant'])
+        self.assertEqual(trie.find_words_start_with('cs'), [])
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
